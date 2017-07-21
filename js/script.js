29a5e91b5835ce562af6de6392911cf5f9b9e43b
Ayesha Kanani <isha@Ayesha-Kananis-MacBook-Pro.local>
2017-07-21 16:48:59 -0700

    updated styles

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+// this is pseudo code to undrstand what needs to be done for the java script to be delted later
+// choose select .onclick
+// header collapse
+// .empty will empty data
+// event prevent default
+// loader
+// json fetch this is where the 12 stories will be look up get jquery get value from select field
+// then fetch ajax make sure only getting 12 stories in a picture use method.filter
+// then put into list in html
+// append data
+// header dissappear
+// .fail
+// there needs to be a function for each step above .append .fail etc
+
+
 $(function() {
   
 });
