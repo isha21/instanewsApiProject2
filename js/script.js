c212f965f32c0d5379e10767fd632ea21736fe16
Ayesha Kanani <isha@Ayesha-Kananis-MacBook-Pro.local>
2017-07-24 16:21:05 -0700

    changed javascript

--- js/script.js
+++ js/script.js
@@ -28,37 +28,38 @@
 
     $('.loader').show();
 
-  var choice =$('.dropdown').val();
+  var choice =$('.dropdown').val(),
       url = 'https://api.nytimes.com/svc/topstories/v2/' + choice + '.json';
       url += '?' + $.param({
-     'api-key': '400551b6bccb49268e4376e6cb6180bc'
+     'api-key': '400551b6bccb49268e4376e6cb6180bc' });
 
-     })
-
-
+  
     $.ajax({
     url: url,
-    method: 'GET'
+    method: 'GET',
+    dataType: 'json'
     })
 
 .done(function(data){
   var newsStories  = ''; 
   var results = data.results.filter(function(value){
-    return value.multimedia.length>=5;
+    return value.multimedia.length >= 5;
   })
   results.splice(12);
 
  $.each(results, function(key,value){
    var images = value.multimedia[4].url,
-       newsstories = value.abstract,
+       words = value.abstract,
        url = value.url;
-  newsStories += '<li>'
 
-  console.log(newsstories);
-})
+       newsStories += '<li>';
+       newsStories += '<a href=' + url + '>';
+       newsStories += '<div class="article" style="background-image: url(' + images + ')">';
+       newsStories += '<p class="words" >';
+       newsStories += words;
+       newsStories += '</p></div></a></li>';    
+      });
 
-.fail(function(err) {
-  throw err;
-    });
+      $('.stories').html(newsStories)
   })
 });
