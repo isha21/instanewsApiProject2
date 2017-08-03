de4c918b831f46f4946456e49b5ce9282ef92b6c
Ayesha Kanani <isha@Ayesha-Kananis-MacBook-Pro.local>
2017-08-02 17:00:09 -0700

    changed dependancies

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,67 @@
 var gulp = require('gulp'),
-  uglify = require('gulp-uglify'),
-  rename = require('gulp-rename'),
-  watch = require('gulp-watch'),
-  browserSync = require('browser-sync').create(),
-  eslint = require('gulp-eslint'),
-  sass = require('gulp-sass'),
-  autoprefixer = require('gulp-autoprefixer'),
-  cssnano = require('gulp-cssnano'),
-  prettyError = require('gulp-prettyerror');
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync').create(),
+    eslint = require('gulp-eslint'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    prettyError = require('gulp-prettyerror'),
+    babel = require('gulp-babel');
 
 // gulp tasks below
-gulp.task('scripts', ['lint'], function () {
-  gulp.src('./js/*.js')
-    .pipe(uglify())
-    .pipe(rename({ extname: '.min.js' }))
-    .pipe(gulp.dest('./build/js'))
-})
-gulp.task('say_hello', function () {
-  console.log('Hello!')
-})
+gulp.task('scripts', ['lint'], function() {
+    gulp.src('./js/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(uglify())
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(gulp.dest('./build/js'))
+});
+
+// gulp.task('say_hello', function() {
+//     console.log('Hello!')
+// })
 
 // reload browser
-gulp.task('browser-sync', function () {
-  browserSync.init({
-    server: {
-      baseDir: './'
-    }
-  }) // end of browser synch init
-  gulp.watch(['build/js/*.js', 'index.html', 'build/css/*.css']).on('change', browserSync.reload) // browser only looks in build folder
+gulp.task('browser-sync', function() {
+    browserSync.init({
+            server: {
+                baseDir: './'
+            }
+        }) // end of browser synch init
+    gulp.watch(['build/js/*.js', 'index.html', 'build/css/*.css']).on('change', browserSync.reload) // browser only looks in build folder
 })
 
 // gulp watch function
-gulp.task('watch', function () {
-  gulp.watch('./js/*.js', ['scripts'])
-  gulp.watch('./sass/*.scss', ['sass'])
-})
+gulp.task('watch', function() {
+    gulp.watch('./js/*.js', ['scripts'])
+    gulp.watch('./sass/*.scss', ['sass'])
+});
 
 // this is lint 
-gulp.task('lint', function () {
-  return gulp.src('./js/*.js')
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError())
-})
+gulp.task('lint', function() {
+    return gulp.src('./js/*.js')
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError())
+});
 
 // this is for scss
 
-gulp.task('sass', function () {
-  gulp.src('./sass/style.scss')
-    .pipe(prettyError())
-    .pipe(sass())
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions']
-    }))
-    .pipe(gulp.dest('./build/css'))
-    .pipe(cssnano())
-    .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('./build/css'))
+gulp.task('sass', function() {
+    gulp.src('./sass/style.scss')
+        .pipe(prettyError())
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('./build/css'))
+        .pipe(cssnano())
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('./build/css'))
 })
 
 
--- js/script.js
+++ js/script.js
@@ -1,55 +1,55 @@
-
 $(function() {
 
-// choose select .onclick
-  $('.dropdown').on('change', function(){
-     event.preventDefault();
-
-     
-// header collapse
-    $('.headercontainer').addClass('headercollapse').removeClass('.headercontainer');
-    $('.logo').addClass('logocollapse').removeClass('.logo');
-    $('.selectionchoice').addClass('selectioncollapse').removeClass('.selectionchoice');
-    
-// loader
-    $('.loader').show();
+    // choose select .onclick
+    $('.dropdown').on('change', function() {
+        event.preventDefault();
 
 
- // json fetch this is where the 12 stories will come from with jquery get value from select field   
-  var choice =$('.dropdown').val(),
-      url = 'https://api.nytimes.com/svc/topstories/v2/' + choice + '.json';
-      url += '?' + $.param({
-     'api-key': '400551b6bccb49268e4376e6cb6180bc' });
+        // header collapse
+        $('.headercontainer').addClass('headercollapse').removeClass('.headercontainer');
+        $('.logo').addClass('logocollapse').removeClass('.logo');
+        $('.selectionchoice').addClass('selectioncollapse').removeClass('.selectionchoice');
 
-  
-    $.ajax({
-    url: url,
-    method: 'GET',
-    dataType: 'json'
+        // loader
+        $('.loader').show();
+
+
+        // json fetch this is where the 12 stories will come from with jquery get value from select field   
+        var choice = $('.dropdown').val(),
+            url = 'https://api.nytimes.com/svc/topstories/v2/' + choice + '.json';
+        url += '?' + $.param({
+            'api-key': '400551b6bccb49268e4376e6cb6180bc'
+        });
+
+
+        $.ajax({
+            url: url,
+            method: 'GET',
+            dataType: 'json'
+        })
+
+        .done(function(data) {
+            var newsStories = '';
+            var results = data.results.filter(function(value) {
+                    return value.multimedia.length >= 5;
+                })
+                // only want 12 stories
+            results.splice(12);
+
+            $.each(results, function(key, value) {
+                var images = value.multimedia[4].url,
+                    words = value.abstract,
+                    url = value.url;
+
+                newsStories += '<li class="textpict">';
+                newsStories += '<a href=' + url + '>';
+                newsStories += '<div class="article" style="background-image: url(' + images + ')">';
+                newsStories += '<p class="words" >';
+                newsStories += words;
+                newsStories += '</p></div></a></li>';
+            });
+
+            $('.stories').html(newsStories)
+        });
     })
-
-.done(function(data){
-  var newsStories  = ''; 
-  var results = data.results.filter(function(value){
-    return value.multimedia.length >= 5;
-  })
-  // only want 12 stories
-  results.splice(12);
-
- $.each(results, function(key,value){
-   var images = value.multimedia[4].url,
-       words = value.abstract,
-       url = value.url;
-
-       newsStories += '<li class="textpict">';
-       newsStories += '<a href=' + url + '>';
-       newsStories += '<div class="article" style="background-image: url(' + images + ')">';
-       newsStories += '<p class="words" >';
-       newsStories += words;
-       newsStories += '</p></div></a></li>';    
-      });
-
-      $('.stories').html(newsStories)
-  });
-  })
 });
